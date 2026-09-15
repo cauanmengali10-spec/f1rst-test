34ba17dab938c9ab6ed3e836eb9a20415c7e5b10
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-15 19:30:08 -0300

    inicial commit

---
+++ src/test/java/com/br/test/f1rst/F1rstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.test.f1rst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1rstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
